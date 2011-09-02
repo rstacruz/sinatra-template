ba09f6b6cb3c193bb46da8a74d30dee7d1322cdb
Rico Sta. Cruz <rico@ambiescent.com>
2011-09-02 12:08:41 +0800

    AssetPack.

--- app/init/js_css.rb
+++ app/init/js_css.rb
@@ -1,17 +1,14 @@
 class Main
-  register Sinatra::JsSupport
-  serve_js '/js', from: root('app/js')
+  register Sinatra::AssetPack
+  assets do
+    js :application, %w[
+      /js/vendor/*.js
+      /js/lib/*.js
+      /js/app/*.js
+    ]
 
-  register Sinatra::CssSupport
-  serve_css '/css', from: root('app/css')
-
-  register Sinatra::CompressedJS
-  serve_compressed_js :app_js,
-    :path   => '/js/app.js',
-    :prefix => '/js',
-    :root   => root('app/js'),
-    :files  =>
-      Dir[root('app/js/vendor/jquery.*.*')].sort +
-      Dir[root('app/js/lib/*.*')].sort +
-      Dir[root('app/js/app/*.*')].sort
+    css :main, %w[
+      /css/style.css
+    ]
+  end
 end
