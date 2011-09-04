9c3a5116ebdf580f2434dc56e322509e78b51613
Rico Sta. Cruz <rico@ambiescent.com>
2011-09-04 09:27:37 +0800

    Chromeframe prompt.

--- app/helpers/js_cdn_helpers.rb
+++ app/helpers/js_cdn_helpers.rb
@@ -12,6 +12,13 @@ class Main
         str
       end
     end
+
+    def js_chromeframe
+      "<!--[if lt IE 7 ]>" +
+      "<script defer src='//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js'></script>" +
+      "<script defer>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>" +
+      "<![endif]-->"
+    end
   end
 
   helpers JsCdnHelpers
